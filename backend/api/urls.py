8f20f5637fff3774873541ddfb9102987ec9e31d
giji676 <tvtengoxachidze@gmail.com>
2025-11-14 21:45:00 +0000

    Urlpattersn

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # House endpoints
     path("house/create/", views.CreateHouseView.as_view(), name="create-house"),
+    path("house/join/<join_code>/", views.JoinHouseView.as_view(), name="join-house"),
+
+    # Address endpoints
     path("address-autocomplete/", views.AddressAutocompleteView.as_view(), name="address-autocomplete"),
     path("address-details/", views.AddressDetailsView.as_view(), name="address-details"),
-    path("house/join/<join_code>/", views.JoinHouseView.as_view(), name="join-house"),
+
+    # Chore endpoints
+    path("chores/create/", views.CreateChoreView.as_view(), name="create-chore"),
+    path("chores/delete/<int:chore_id>/", views.DeleteChoreView.as_view(), name="delete-chore"),
+    path("chores/assign/", views.AssignChoreView.as_view(), name="assign-chore"),
+    path("chores/assignment/<int:assignment_id>/", views.UpdateChoreAssignmentView.as_view(), name="update-chore-assignment"),
+    path("chores/assignment/<int:assignment_id>/delete/", views.DeleteChoreAssignmentView.as_view(), name="delete-chore-assignment"),
 ]
