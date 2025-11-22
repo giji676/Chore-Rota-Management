09410c0fdaac83c5c7878eaaa00f939183b2fe3a
giji676 <tvtengoxachidze@gmail.com>
2025-11-22 17:45:34 +0000

    Set up admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import House, HouseMember, Chore, Rota, ChoreAssignment
 
-# Register your models here.
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "address", "join_code", "max_members")
+    search_fields = ("name", "address", "join_code")
+
+@admin.register(HouseMember)
+class HouseMemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "house", "role", "joined_at")
+    list_filter = ("role", "house")
+    search_fields = ("user__username",)
+
+@admin.register(Chore)
+class ChoreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "house")
+    list_filter = ("house",)
+    search_fields = ("name",)
+
+@admin.register(Rota)
+class RotaAdmin(admin.ModelAdmin):
+    list_display = ("id", "house", "start_date", "end_date", "created_at")
+    list_filter = ("house", "start_date")
+
+@admin.register(ChoreAssignment)
+class ChoreAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "rota", "chore", "person", "day", "completed")
+    list_filter = ("rota", "day", "completed")
+    search_fields = ("chore__name", "person__username")
