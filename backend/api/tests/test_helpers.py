8bd1cc97e8755892601c97cb3735948ac7ba1a92
giji676 <tvtengoxachidze@gmail.com>
2026-01-01 20:29:17 +0000

    DateTimeParser test start

--- backend/api/tests/test_helpers.py
+++ backend/api/tests/test_helpers.py
@@ -2,6 +2,7 @@ import pytest
 from django.test import TestCase
 
 from api.helpers.generic_utils import check_version
+from api.helpers.parse_datetime import parse_client_datetime
 from api.exceptions import Conflict
 
 
@@ -38,3 +39,14 @@ class TestCheckVersion(TestCase):
         obj = VersionedObject(version=3)
         # should not raise
         check_version(obj, client_version=3)
+
+from django.utils import timezone
+import datetime
+
+class TestParseDateTime(TestCase):
+    def test_(self):
+        print(timezone.now())
+        print(datetime.datetime.now())
+        # dt_str = "2024-06-01T12:00:00Z"
+        # dt = parse_client_datetime(dt_str)
+        # self.assertEqual(dt.isoformat(), "2024-06-01T12:00:00+00:00")
