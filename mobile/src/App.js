8e1a3b8bdf86aa131184dd7a4543b8aba0c70396
giji676 <tvtengoxachidze@gmail.com>
2026-01-13 17:45:40 +0000

    Actions module install

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,11 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from './navigation/NavigationService';
 import MainStack from './navigation/MainStack';
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <MainStack />
-        </NavigationContainer>
+        <ActionSheetProvider>
+            <NavigationContainer>
+                <MainStack />
+            </NavigationContainer>
+
+        </ActionSheetProvider>
     );
 }
